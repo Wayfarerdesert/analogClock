bf473ba1bba530eac38e225f0d357fd57920013e
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2025-01-13 01:29:19 +0100

    clock numbers straightened

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
 
+function numbers() {
+    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const numbers = document.querySelector('.numberWrapper');
+
+    num.forEach((num) => {
+        const div = document.createElement('div');
+        div.classList.add('number', 'number' + num);
+
+        const span = document.createElement('span');
+        span.classList.add('number' + num + 'container');
+        span.innerText = num;
+
+        div.appendChild(span);
+        numbers.appendChild(div);
+    });
+}
+
+numbers();
+
 function setClock() {
     const currentDate = new Date();
     const secondsRatio = currentDate.getSeconds() / 60;
